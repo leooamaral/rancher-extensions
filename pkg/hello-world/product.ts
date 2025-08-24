27ecf97afae2831bdecf47132d3760a028a69ca6
Leonardo Miranda Amaral <leonardo_miranda@icloud.com>
2025-08-24 16:56:36 -0300

    feat: add

--- pkg/hello-world/product.ts
+++ pkg/hello-world/product.ts
@@ -5,35 +5,22 @@ export function init($plugin: IPlugin, store: any) {
   const BLANK_CLUSTER = '_';
   const CUSTOM_PAGE_NAME = 'page1';
 
-  const { product, virtualType, basicType } = $plugin.DSL(store, YOUR_PRODUCT_NAME);
+  const { product, basicType } = $plugin.DSL(store, YOUR_PRODUCT_NAME);
 
   product({
     icon: 'gear',
     inStore: 'management',
     weight: 100,
     to: {
-      name: `${YOUR_PRODUCT_NAME}-c-cluster`,
-      path: `/${YOUR_PRODUCT_NAME}/c/:cluster/dashboard`,
+      name: `${ YOUR_PRODUCT_NAME }-c-cluster-${ CUSTOM_PAGE_NAME }`,
+      path: `/${YOUR_PRODUCT_NAME}/c/:cluster/${ CUSTOM_PAGE_NAME }`,
       params: {
         product: YOUR_PRODUCT_NAME,
         cluster: BLANK_CLUSTER,
-        pkg: YOUR_PRODUCT_NAME,
       },
     },
   });
 
-  // creating a custom page
-  virtualType({
-    labelKey: 'some.translation.key',
-    name:     CUSTOM_PAGE_NAME,
-    route:    {
-      name:   `${ YOUR_PRODUCT_NAME }-c-cluster-${ CUSTOM_PAGE_NAME }`,
-      params: {
-        product: YOUR_PRODUCT_NAME,
-        cluster: BLANK_CLUSTER
-      }
-    }
-  });
   // registering the defined pages as side-menu entries
   basicType([CUSTOM_PAGE_NAME]);
 }
