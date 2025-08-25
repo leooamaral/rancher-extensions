955820ead264ac64ceada85ea1477a600b0d0685
Leonardo Miranda Amaral <leonardo_miranda@icloud.com>
2025-08-24 22:12:23 -0300

    add

--- pkg/hello-world/pages/index.vue
+++ pkg/hello-world/pages/index.vue
@@ -1,14 +1,60 @@
 <script>
-export default { name: 'MyCustomPage' };
+import { STEVE } from '@rancher/shell/plugins/steve';
+
+export default {
+  name: 'ClusterList',
+
+  data() {
+    return {
+      clusters: []
+    };
+  },
+
+  created() {
+    // Start watching clusters
+    this.$store.dispatch(`${STEVE}/watch`, {
+      type: 'management.cattle.io.cluster',
+      watch: (event) => {
+        // event has: { type: 'ADDED'|'MODIFIED'|'DELETED', data: clusterObj }
+        if (event.type === 'ADDED') {
+          this.clusters.push(event.data);
+        } else if (event.type === 'MODIFIED') {
+          this.clusters = this.clusters.map(c =>
+            c.id === event.data.id ? event.data : c
+          );
+        } else if (event.type === 'DELETED') {
+          this.clusters = this.clusters.filter(c => c.id !== event.data.id);
+        }
+      }
+    });
+  },
+
+  computed: {
+    activeClusters() {
+      return this.clusters.filter(c => {
+        // ✅ Keep local + downstream clusters
+        // Only show Active/Ready
+        if (c?.status?.phase) {
+          return c.status.phase.toLowerCase() === 'active';
+        }
+
+        const readyCondition = c?.status?.conditions?.find(
+          cond => cond.type === 'Ready'
+        );
+        return readyCondition?.status === 'True';
+      });
+    }
+  }
+};
 </script>
 
 <template>
   <div>
-    <h1>Top Level Product</h1>
-    <p>This is a custom VueJS page. You can render anything you want</p>
+    <h2>Active Clusters (Live)</h2>
+    <ul>
+      <li v-for="c in activeClusters" :key="c.id">
+        {{ c.spec?.displayName || c.metadata?.name }}
+      </li>
+    </ul>
   </div>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
