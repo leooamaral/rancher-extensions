181062474d136bc787571adcd684bccff74525b5
Leonardo Miranda Amaral <leonardo_miranda@icloud.com>
2025-08-24 23:11:11 -0300

    add

--- pkg/hello-world/pages/index.vue
+++ pkg/hello-world/pages/index.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
     <h2>Clusters</h2>
+    <pre>{{ clusters }}</pre>
+  </div>
+</template>
+
+<script>
+import { MANAGEMENT } from '@shell/config/types';
+
+export default {
+  data() {
+    return {
+      clusters: []
+    }
+  },
+  async created() {
+    try {
+      const res = await this.$store.dispatch('management/findAll', { type: MANAGEMENT.CLUSTER, });
+      this.clusters = res;
+      console.log('Clusters:', res);
+    } catch (err) {
+      console.error('Failed to load clusters', err);
+    }
+  }
+}
+</script>
+
+<!-- <template>
+  <div>
+    <h2>Clusters</h2>
 
     <ul>
       <li v-for="c in clusters" :key="c.id">
@@ -74,4 +102,4 @@ export default {
     }
   }
 };
-</script>
+</script> -->
